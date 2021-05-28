6eea052fecf13794e420ea014c814e783753bd9d
Ricardo Rossini <ricardo.rossini.dev@outlook.com>
2021-05-28 01:48:46 -0300

    adding Icons

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var sidenav = document.querySelectorAll('.sidenav');
-    var modals = document.querySelectorAll('.modal');
-    var fab = document.querySelectorAll('.fixed-action-btn');
-    M.FloatingActionButton.init(fab);
-    M.Sidenav.init(sidenav);
-    M.Modal.init(modals);
-  });
+  var sidenav = document.querySelectorAll('.sidenav');
+  var modals = document.querySelectorAll('.modal');
+  var fab = document.querySelectorAll('.fixed-action-btn');
+  M.FloatingActionButton.init(fab);
+  M.Sidenav.init(sidenav);
+  M.Modal.init(modals);
+});
